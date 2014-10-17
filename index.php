7d48a2bd26cbba76f09ea82c5cb36d7a4f8d0131
Preben Berg <preben.berg@veeam.com>
2014-10-17 15:18:47 +0200

    Added Twitter Bootstrap to form and composer.json

--- index.php
+++ index.php
@@ -9,11 +9,15 @@ require 'vendor/autoload.php';
 <html>
   <head>
     <title>Veeam RESTful demo</title>
-    <script type="text/javascript" src="vendor/components/jquery/jquery.min.js"></script>
-    <script type="text/javascript" src="vendor/components/jqueryui/jquery-ui.min.js"></script>
+    <!--<script type="text/javascript" src="vendor/components/jquery/jquery.min.js"></script>-->
+    <link rel="stylesheet" type="text/css" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="vendor/twbs/bootstrap/dist/css/bootstrap-theme.min.css" />
+    <script type="text/javascript" src="components/jquery/jquery.min.js"></script>
+    <script type="text/javascript" src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
     <script type="text/javascript">
     $(document).ready(function() {
       $(document).ajaxStart(function(){
+        $('#form_container').hide();
         $('#loading').show();
       });
       
@@ -23,7 +27,7 @@ require 'vendor/autoload.php';
     
       $('#veeam_cloud_connect_form').submit(function() {
         $.post($(this).attr('action'), $(this).serialize(), function(json) {
-          $('#result').html('<h1>Username: ' + json.username + '</h1><br /><h1>Password: ' + json.password + '</h1>')
+          $('#result').html('<p class="bg-success"> <h1>Username: ' + json.username + '</h1> <h1>Password: ' + json.password + '</h1> </p>')
         }, 'json');
         return false;
       });
@@ -33,21 +37,34 @@ require 'vendor/autoload.php';
     <link rel="stylesheet" 
   </head>
   <body>
-  
-  <form id="veeam_cloud_connect_form" method="POST" action="/veeam.php">
-    <input type="hidden" name="action" value="create_tenant" />
-    <label for="username">Username</label>
-    <input type="text" name="username" value="preben" />
-    <label for="full_name">Full name</label>
-    <input type="text" name="full_name" value="Preben Berg" />
-    <label for="company_name">Company name</label>
-    <input type="text" name="company_name" value="Veeam" />
-    <input type="submit" value="Create account" />
-  </form>
-  
-  <div id="loading" style="display:none;"><h1>LOADING</h1></div>
-  
-  <div id="result"></div>
-  
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-9">
+        <p class="text-center"><img src="veeam_logo.png" align="center"/></p>
+        <p class="text-center lead"><b>RESTful API demo for Veeam Cloud Connect</b><br />Type in your information below to create Cloud Repository with 100 GB quota.</p>
+        <div id="form_container">
+          <form id="veeam_cloud_connect_form" method="POST" action="/veeam.php" role="form">
+            <input type="hidden" name="action" value="create_tenant" />
+            <div class="form-group">
+              <label for="username">Username</label>
+              <input type="text" name="username" class="form-control" placeholder="Enter desired username" />
+            </div>
+            <div class="form-group">
+              <label for="full_name">Full name</label>
+              <input type="text" name="full_name" class="form-control" placeholder="Enter full name" />
+            </div>
+            <div class="form-group">
+              <label for="company_name">Company name</label>
+              <input type="text" name="company_name" class="form-control" placeholder="Enter company name" />
+            </div>
+            <button type="submit" class="btn btn-default">Create account</button>
+          </form>
+        </div>
+        <div id="loading" style="display:none;"><h1>LOADING</h1></div>
+        
+        <div id="result"></div>
+      </div>
+    </div>
+  </div>
   </body>  
 </html>
