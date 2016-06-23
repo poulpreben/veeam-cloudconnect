0726047e13490941f39fc652ac5214e7eb6a7e84
preben <preben.berg@veeam.com>
2016-06-23 10:54:28 +0200

    Changed config parameters to variables

--- veeam.php
+++ veeam.php
@@ -10,12 +10,18 @@ require 'veeam.class.php';
 header('Content-Type: application/json');
 
 if (isset($_POST['action']) && (isset($_POST['create_backup']) || isset($_POST['create_replication'])) && $_POST['action'] == "create_tenant") {
+
+
+  $rest_server = 'REPLACE_SERVER';
+  $rest_port   = 'REPLACE_PORT';
+  $rest_user   = 'REPLACE_USER';
+  $rest_pass   = 'REPLACE_PASS';
   
   // Not very clean, but it works(tm)
   $username = strtolower($_POST['username']);
   $description = $_POST['full_name'] . " - " . $_POST['company_name'];
   
-  $veeam = new Veeam('10.0.0.11', 9399, 'VEEAM-VBR01\\Administrator', '***', $_POST['create_backup'], $_POST['create_replication']);
+  $veeam = new Veeam($rest_server, $rest_port, $rest_user, $rest_pass, $_POST['create_backup'], $_POST['create_replication']);
 
   // Create a user with 10 GB quota and leave them enabled.
   $veeam->run($username, $description, 10240, TRUE);
